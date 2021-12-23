ad5513e6478ee0999c1463ec205c848702887558
Alexandre Ghelfi <alexandreghelfi@gmail.com>
2021-12-23 18:09:17 +0100

    Removing print in the app_conf.py file

--- app/app_conf.py
+++ app/app_conf.py
@@ -28,8 +28,6 @@ root_dir = Path(current_filename).parent.parent
 assets_dir = os.path.join(root_dir, 'assets')
 if not os.path.isdir(assets_dir):
     raise ValueError(f'{assets_dir} does not exists')
-else:
-    print(f"assets_folders set as {assets_dir}")
 dash_app = dash.Dash(__name__, assets_folder=assets_dir)
 
 dash_app.server.config['SQLALCHEMY_DATABASE_URI'] = os.path.join('sqlite:///'+os.getcwd(), 'test.db')
